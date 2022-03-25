d3afdba99c74d19de674cccc91feffabc5dbe5a9
shibamirai <66295034+shibamirai@users.noreply.github.com>
2022-03-25 13:49:24 +0900

    パッケージ構成の見直し

--- src/main/java/katachi/jobfinders/controller/LoginController.java
+++ src/main/java/katachi/jobfinders/controller/LoginController.java
@@ -1,4 +1,4 @@
-package katachi.jobfinders.controller.login;
+package katachi.jobfinders.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
